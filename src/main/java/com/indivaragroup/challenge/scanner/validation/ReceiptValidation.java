fd5bb4dc98b84e6b4e20bcfa79c888c41a330bcb
princeofverry <verryk49@gmail.com>
2026-05-26 20:53:11 +0700

    feat: chore day 11

--- src/main/java/com/indivaragroup/challenge/scanner/validation/ReceiptValidation.java
+++ src/main/java/com/indivaragroup/challenge/scanner/validation/ReceiptValidation.java
@@ -4,11 +4,8 @@ import java.util.regex.Pattern;
 
 public class ReceiptValidation {
 
-    private static final Pattern QUANTITY_REGEX =
-            Pattern.compile("^[1-9][0-9]*$");
-
-    private static final Pattern PRICE_REGEX =
-            Pattern.compile("^\\d+(\\.\\d{1,2})?$");
+    private static final Pattern QUANTITY_REGEX = Pattern.compile("^[1-9][0-9]*$");
+    private static final Pattern PRICE_REGEX = Pattern.compile("^\\d+(\\.\\d{1,2})?$");
 
     public static void validateQuantity(
             String quantity
